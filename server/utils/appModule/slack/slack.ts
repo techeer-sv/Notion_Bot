c943127bd14d41907bae3fa2372a3435edaed59b
ppinkypeach <mira20932@gmail.com>
2024-01-16 22:17:22 +0900

    fix: slackApi socket mode 해제

--- server/utils/appModule/slack/slack.ts
+++ server/utils/appModule/slack/slack.ts
@@ -3,6 +3,4 @@ import { App } from '@slack/bolt';
 export const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
-    appToken: process.env.SLACK_APP_TOKEN, 
-    socketMode: true,
 });
