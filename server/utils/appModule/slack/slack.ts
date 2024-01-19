1546f2ba3ef206d9631cca36cf242bb2504ba734
ppinkypeach <mira20932@gmail.com>
2024-01-20 01:50:22 +0900

    refactor: 제거

--- server/utils/appModule/slack/slack.ts
+++ server/utils/appModule/slack/slack.ts
@@ -4,5 +4,4 @@ import { awsLambdaReceiver } from '../lambdaReciver/awsLambdaReceiver';
 export const app = new App({
     token: process.env.SLACK_BOT_TOKEN as string,
     receiver: awsLambdaReceiver,
-    // signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
