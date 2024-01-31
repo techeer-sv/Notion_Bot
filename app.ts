792f4a099261ac20dbc6a00e695eeafbd559eb2f
ppinkypeach <mira20932@gmail.com>
2024-01-31 23:41:23 +0900

    refactor: 초기화 로직 변경으로 인한 app.ts 삭제

--- app.ts
+++
@@ -1,6 +0,0 @@
-import {submitNotionCommandHandler} from './server/notion/blog/handler/submitNotionCommandHandler';
-import {uploadNotionBlogViewHandler} from './server/notion/blog/handler/uploadNotionBlogViewHandler';
-import { app } from './server/utils/appModule/slack/slack';
-
-submitNotionCommandHandler(app);
-uploadNotionBlogViewHandler(app);
