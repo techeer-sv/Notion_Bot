8c6074d9b58821af9560d3cd83bbb1b8e7dc345f
ppinkypeach <mira20932@gmail.com>
2024-02-01 11:55:55 +0900

    fix: 중복 초기화로 인한 문제 해결

--- server/utils/function/lambdaHandler/handler.ts
+++ server/utils/function/lambdaHandler/handler.ts
@@ -5,13 +5,19 @@ import { uploadNotionBlogViewHandler } from "../../../notion/blog/handler/upload
 import { app } from "../../appModule/slack/slack";
 import { notion } from "../../appModule/notion/notion";
 
+let handlerRegistered = false;
+
 export const handler = async(
     event: AwsEvent,
     context: any,
     callback: AwsCallback
 ): Promise<AwsResponse> => {
-    submitNotionCommandHandler(app);
-    uploadNotionBlogViewHandler(app, notion);
+    if(!handlerRegistered){
+        submitNotionCommandHandler(app);
+        uploadNotionBlogViewHandler(app, notion);
+        handlerRegistered = true;
+        console.log('lambda에 handler 등록');
+    }
     const slackHandler = await awsLambdaReceiver.start();
     console.log('awsLambdaReceiver 실행');
     return slackHandler(event, context, callback);
